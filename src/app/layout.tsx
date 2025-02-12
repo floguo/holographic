47b7b6f99305cd9708b9411df8059313f94cbe73
floguo <floraguolr@gmail.com>
2025-02-12 09:37:37 -0500

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "floguo × hologram",
+  title: "floguo hologram",
   description: "incoming at vercel design",
 };
 
